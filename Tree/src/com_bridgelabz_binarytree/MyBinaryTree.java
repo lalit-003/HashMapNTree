8b424324c26981422d51f897f08e8c1c8a4e8dcc
lalit-003 <gahlawatl7@gmail.com>
2020-10-13 00:13:37 +0530

    Updating Binary tree to take multiple values

--- Tree/src/com_bridgelabz_binarytree/MyBinaryTree.java
+++ Tree/src/com_bridgelabz_binarytree/MyBinaryTree.java
@@ -38,13 +38,33 @@ public class MyBinaryTree<K extends Comparable<K>> {
 
 	public static void main(String[] args)
 	{
-		MyBinaryTree<Integer> binaryTree = new MyBinaryTree<>();
-		   binaryTree.add(56);
-		   binaryTree.add(30);
-		   binaryTree.add(70);
-		   int size = binaryTree.getSize();
-		   System.out.println("Size is :"+ size);
-		   Assert.assertEquals(3, size);
+//		// test-1 three elements added to tree
+//		MyBinaryTree<Integer> binaryTree = new MyBinaryTree<>();
+//		   binaryTree.add(56);
+//		   binaryTree.add(30);
+//		   binaryTree.add(70);
+//		   int size = binaryTree.getSize();
+//		   System.out.println("Size is :"+ size);
+//		   Assert.assertEquals(3, size);
+		   
+		// test-2 multiple elements added to tree
+			MyBinaryTree<Integer> binaryTree = new MyBinaryTree<>();
+			   binaryTree.add(56);
+			   binaryTree.add(30);
+			   binaryTree.add(70);
+			   binaryTree.add(22);
+				binaryTree.add(40);
+				binaryTree.add(60);
+				binaryTree.add(95);
+				binaryTree.add(11);
+				binaryTree.add(3);
+				binaryTree.add(16);
+				binaryTree.add(65);
+				binaryTree.add(63);
+				binaryTree.add(67);
+			   int size = binaryTree.getSize();
+			   System.out.println("Size is :"+ size);
+			   Assert.assertEquals(13, size);
 
 	}
 }
